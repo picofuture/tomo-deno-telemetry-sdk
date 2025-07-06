8833fa48e9cc813358dc28b0f009c5a04aba0992
Anfal <muhammadanfal0@gmail.com>
2025-07-06 15:37:02 -0700

    Fix resources

--- src/otel/tracers.js
+++ src/otel/tracers.js
@@ -1,7 +1,7 @@
 import { trace } from 'npm:@opentelemetry/api@1.9.0'
 import { BasicTracerProvider, BatchSpanProcessor, ConsoleSpanExporter } from 'npm:@opentelemetry/sdk-trace-base@2.0.1'
 import { OTLPTraceExporter } from 'npm:@opentelemetry/exporter-trace-otlp-http@0.202.0'
-import { resourceFromAttributes, defaultResource } from 'npm:@opentelemetry/resources@2.0.1'
+import { resourceFromAttributes } from 'npm:@opentelemetry/resources@2.0.1'
 import { SemanticResourceAttributes } from 'npm:@opentelemetry/semantic-conventions@1.34.0'
 
 let tracer
@@ -9,10 +9,13 @@ let tracer
 function setupTracer(serviceName, serviceVersion, apiKey, collectorUrl) {
   const name = serviceName || 'unknown_service'
   const version = serviceVersion || '0.0.1'
-  const resource = defaultResource().merge(resourceFromAttributes({
+  const resource = resourceFromAttributes({
     [SemanticResourceAttributes.SERVICE_NAME]: name,
-    [SemanticResourceAttributes.SERVICE_VERSION]: version
-  }))
+    [SemanticResourceAttributes.SERVICE_VERSION]: version,
+    [SemanticResourceAttributes.TELEMETRY_SDK_LANGUAGE]: 'deno',
+    [SemanticResourceAttributes.TELEMETRY_SDK_NAME]: 'tomo-deno-telemetry-sdk',
+    [SemanticResourceAttributes.TELEMETRY_SDK_VERSION]: '0.1.14'
+  })
   const traceExporter = new OTLPTraceExporter({
     url: collectorUrl,
     headers: {
